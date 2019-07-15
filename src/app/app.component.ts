33c36161cf36b094e95b119ac9d4e13262868353
Xavigh <fgargallo@gmail.com>
2019-07-15 14:21:40 +0200

    1
    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto001';
+  nombre = 'Rodriguez Pablo';
+  edad = 7;
+  email = 'rpablo@gmail.com';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+
+  esActivo() {
+    if (this.activo)
+      return 'Trabajador Activo';
+    else
+      return 'Trabajador Inactivo';
+  }
+
+  ultimos3Sueldos() {
+    let suma=0;
+    for(let x=0; x<this.sueldos.length; x++)
+      suma+=this.sueldos[x];
+    return suma;
+  }
 }
